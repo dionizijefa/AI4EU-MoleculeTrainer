8193599b5cee07eda3e0e961ef775501af24ba76
dionizije <dionizije.fa@hotmail.com>
2021-11-30 12:49:32 +0100

    Fix loading of model state

--- example_client.py
+++ example_client.py
@@ -3,6 +3,7 @@ from lightning_trainer import EGConvNet
 from utils import create_loader, standardise_dataset, task_type
 from pathlib import Path
 import pandas as pd
+from torch import load
 
 
 def main():
@@ -40,18 +41,26 @@ def main():
     )
     """
     name="test"
-    # Evaluate on the test set
-    withdrawn_model_path = Path('./model/{}/checkpoint/'.format(name))
-    files = withdrawn_model_path.glob(r'**/*.ckpt')
-    files = [i for i in files]
-    print(files)
-    model = EGConvNet.load_from_checkpoint(checkpoint_path=files[0])
-    model.eval()  # set this flag when doing inference, not required when running model.test
 
+    """Evalaute the model on the test set"""
+    #load the data
     test = pd.read_csv('./data/test.csv')
     problem = task_type(test, target_col)
     test = standardise_dataset(test, smiles_col)  # don't forget to standardise the test set smiles
     test_loader = create_loader(test, target_col, batch_size=batch_size)
+
+    #load the modal
+    withdrawn_model_path = Path('./model/{}/checkpoint/'.format(name))
+    files = withdrawn_model_path.glob(r'**/*.ckpt')
+    files = [i for i in files]
+    checkpoint = load(files)
+    hparams = checkpoint['hparams']
+    state_dict = checkpoint['state_dict']
+    model = EGConvNet(problem=problem, hparams=hparams)
+    model.load_state_dict(state_dict)
+    model.eval()  # set this flag when doing inference, not required when running model.test
+
+    #run the test phase
     result = model.test(test_loader)
     print(result[0])
 
